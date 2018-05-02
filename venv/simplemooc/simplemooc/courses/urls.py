b353784f24eca30819128f904a125d5104c52727
Vinicius Vianna <vinicrit@gmail.com>
2018-05-02 00:20:27 -0300

    Created first course details page

--- venv/simplemooc/simplemooc/courses/urls.py
+++ venv/simplemooc/simplemooc/courses/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from simplemooc.courses import views
 
 app_name = 'courses'
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	re_path(r'^(?P<pk>\d+)/$', views.details, name='details'),
 ]
